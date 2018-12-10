e0a25531a6641678bafcee23c481b23807cd1698
daisybaicai <360619895@qq.com>
2018-12-10 22:20:08 +0800

    models
    models

--- src/store/modules/dongtai.js
+++ src/store/modules/dongtai.js
@@ -17,14 +17,14 @@ const mutations = {
 
 const actions = {
   getDongtai ({ state, commit }) {
-    axios.get('http://localhost:8080/static/dongtai.json').then((res) => {
+    axios.get('http://localhost:8081/static/dongtai.json').then((res) => {
       this.res = res
       commit('SET_CURRENT_DONGTAI_LISTS', { dongtaiList: res.data.dongtaiList })
       console.log(res.data.dongtaiList)
     })
   },
   sendComment ({state, commit}) {
-    axios.post('http://localhost:8080/static/dongtai.json').then((res) => {
+    axios.post('http://localhost:8081/static/dongtai.json').then((res) => {
       this.res = res
     })
   },
